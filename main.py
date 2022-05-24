3c289d7fed060ec431556ba99da5ed6eb6606fcd
ADS\ga97run <jonathan.rapp33@gmail.com>
2022-05-24 10:05:51 +0200

    no message

--- main.py
+++ main.py
@@ -221,8 +221,9 @@ class Main(QMainWindow, Ui_MainWindow):
                 export_dict[z].append(current)
         df = pd.DataFrame(export_dict)
         today = date.today()
-        df.to_csv(str(today) + 'vagus_fo8_diam_vs_z_offset.csv', index=False, header=True)
+        df.to_csv(str(today) + 'phrenic_fo850%_diam_vs_z_offset.csv', index=False, header=True)
         print('Finished!')
+
 
 
 
